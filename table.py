4be6923418563bc53891c77cd18be56432cf2d38
Vlek6 <104684306+Vlek6@users.noreply.github.com>
2023-03-10 19:02:42 +0100

    found first working regex recipe for finding values of parameteres

--- table.py
+++ table.py
@@ -1,9 +1,14 @@
 import pandas as pd
-
+import re
 import main
 from pprint import pprint
 data = pd.read_csv('log.csv', sep=',', header=None)
 parametersIndexRange = data.index[data[0] == '<CONTROLLER_CONFIG>']
 extractedParameters = data.iloc[parametersIndexRange[0]:parametersIndexRange[1], 0:2]
-print(extractedParameters)
-
+# print(extractedParameters.head(20))
+print(extractedParameters.iloc[56,1])
+# for i in enumerate(extractedParameters):
+#     num = re.findall(extractedParameters.iloc[i])
+#     print(num)
+print(extractedParameters.iloc[96, 1])
+print(re.findall('(\d+\.\d+)', extractedParameters.iloc[96, 1]))
