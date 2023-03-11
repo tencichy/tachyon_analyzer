1728c22da1e1ebd59a6801f66b28b962a921f6d9
Vlek6 <104684306+Vlek6@users.noreply.github.com>
2023-03-11 14:25:13 +0100

    Tweaked regex expression. Implemented class for storing parameters. Implemented function for collecting parameters from files csv files. Cleaned-up whole file.

--- table.py
+++ table.py
@@ -1,23 +1,32 @@
 import pandas as pd
 import re
-import math
-import main
-from pprint import pprint
 
 
-def isnan(string):
-    return string != string
+class Parameter:
+    def __init__(self, in_tuple):
+        self.composed = in_tuple[0]
+        self.value = in_tuple[1]
+        self.unit = in_tuple[2]
 
-data = pd.read_csv('log.csv', sep=',', header=None)
-parametersIndexRange = data.index[data[0] == '<CONTROLLER_CONFIG>']
-extractedParameters = data.iloc[parametersIndexRange[0]:parametersIndexRange[1], 0:2]
-# print(extractedParameters.head(20))
-# print(extractedParameters.iloc[56, 1])
-for index, row in extractedParameters.iterrows():
-    if isnan(row[1]) is False:
-        num = re.findall('(?<=\w.: )(\d+\.*\d*)', row[1])
-        print(num)
+    def __str__(self):
+        return f"{self.composed},{self.value},{self.unit}"
 
-# print(extractedParameters.iloc[96, 1])
-# print(re.findall('(\d+\.\d+)', extractedParameters.iloc[1, 1]))
-# '(\d+\.\d+)'
+    def __repr__(self):
+        return "[% s, % s, % s]" % (self.composed, self.value, self.unit)
+
+
+def get_parameters(filename):
+    def isnan(string):
+        return string != string
+
+    data = pd.read_csv('log.csv', sep=',', header=None)
+    parameters_index_range = data.index[data[0] == '<CONTROLLER_CONFIG>']
+    extracted_parameters = data.iloc[parameters_index_range[0]:parameters_index_range[1], 0:2]
+
+    params = []
+    for index, row in extracted_parameters.iterrows():
+        if isnan(row[1]) is False:
+            num = re.findall('(?<=[a-zA-Z].: )((\d+\.*\d*)([a-zA-Z]{0,3}))', row[1])
+            if len(num) > 0:
+                params.append(Parameter(num[0][0:3]))
+    return params
