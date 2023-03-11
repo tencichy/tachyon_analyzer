54a9b433baac2a31869efcb1fab9daac5bc8334c
Vlek6 <104684306+Vlek6@users.noreply.github.com>
2023-03-11 12:38:51 +0100

    fixed regex expression for searching numeric values

--- table.py
+++ table.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import re
+import math
 import main
 from pprint import pprint
+
+
+def isnan(string):
+    return string != string
+
 data = pd.read_csv('log.csv', sep=',', header=None)
 parametersIndexRange = data.index[data[0] == '<CONTROLLER_CONFIG>']
 extractedParameters = data.iloc[parametersIndexRange[0]:parametersIndexRange[1], 0:2]
 # print(extractedParameters.head(20))
-print(extractedParameters.iloc[56,1])
-# for i in enumerate(extractedParameters):
-#     num = re.findall(extractedParameters.iloc[i])
-#     print(num)
-print(extractedParameters.iloc[96, 1])
-print(re.findall('(\d+\.\d+)', extractedParameters.iloc[96, 1]))
+# print(extractedParameters.iloc[56, 1])
+for index, row in extractedParameters.iterrows():
+    if isnan(row[1]) is False:
+        num = re.findall('(?<=\w.: )(\d+\.*\d*)', row[1])
+        print(num)
+
+# print(extractedParameters.iloc[96, 1])
+# print(re.findall('(\d+\.\d+)', extractedParameters.iloc[1, 1]))
+# '(\d+\.\d+)'
