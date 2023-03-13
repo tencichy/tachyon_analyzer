dd4ff2ccd65d5631ea29abae5443b74aa2ea817d
Vlek6 <104684306+Vlek6@users.noreply.github.com>
2023-03-13 09:02:33 +0100

    Change datatype of return in get_parameters to dataframe
    Change in get_parameters() function - datatype of return value is now a Pandas DataFrame.
    Moreover as soon as this modification is applied, the parameters' class is redundant and
    was deleted.

--- parameters.py
+++ parameters.py
@@ -1,25 +1,13 @@
-import pandas as pd
 import re
 
-
-class Parameter:
-    def __init__(self, in_tuple):
-        self.composed = in_tuple[0]
-        self.value = in_tuple[1]
-        self.unit = in_tuple[2]
-
-    def __str__(self):
-        return f"{self.composed},{self.value},{self.unit}"
-
-    def __repr__(self):
-        return "[% s, % s, % s]" % (self.composed, self.value, self.unit)
+import pandas as pd
 
 
 def get_parameters(filename):
     def isnan(string):
         return string != string
 
-    data = pd.read_csv('log.csv', sep=',', header=None)
+    data = pd.read_csv(filename, sep=',', header=None)
     parameters_index_range = data.index[data[0] == '<CONTROLLER_CONFIG>']
     extracted_parameters = data.iloc[parameters_index_range[0]:parameters_index_range[1], 0:2]
 
@@ -28,5 +16,6 @@ def get_parameters(filename):
         if isnan(row[1]) is False:
             num = re.findall('(?<=[a-zA-Z].: )((\d+\.*\d*)([a-zA-Z]{0,3}))', row[1])
             if len(num) > 0:
-                params.append(Parameter(num[0][0:3]))
-    return params
+                params.append(num[0][0:3])
+    params_df = pd.DataFrame(params, columns=['Compound', 'Value', 'Unit'])
+    return params_df
